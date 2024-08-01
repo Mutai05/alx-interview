08e495e16a3baf88e39e8e1edf5e654c72e55a7a
Mutai05 <kelvok2014@gmail.com>
2024-07-31 17:10:37 -0700

    Fixed pycodestyle format

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,15 +2,18 @@
 """
 0-island_perimeter
 """
+
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island in a grid.
-    
+
     Args:
-    grid (list of list of int): 2D list representing the grid where 1's are land and 0's are water.
-    
+        grid (list of list of int): 2D list representing the grid where 1's
+                                    are land and 0's are water.
+
     Returns:
-    int: The perimeter of the island.
+        int: The perimeter of the island.
     """
     if not grid or not grid[0]:
         return 0
